87d04994b0e1fc84ea9845254e611e0b5231cc78
scarpino <scarpino@utexas.edu>
2020-02-25 14:42:23 -0500

    adding 10 day windows for model selection

--- code/model_selection_scenarios.R
+++ code/model_selection_scenarios.R
@@ -22,8 +22,8 @@ library(glmnet)
 #Global Params#
 ###############
 last_date <- as.POSIXct(strptime("2020-02-18", format = "%Y-%m-%d")) #date when line list is most up-to-date
-first_date <- as.POSIXct(strptime("2020-01-01", format = "%Y-%m-%d")) #date to start recording cases
-dates <- seq(from = first_date, to = last_date, by = 60*60*24)
+first_date <- as.POSIXct(strptime("2020-01-03", format = "%Y-%m-%d")) #date to start recording cases
+dates <- seq(from = first_date, to = last_date, by = 60*60*24*10)
   
 save_new <- FALSE
 time_stamp <- as.numeric(Sys.time())
@@ -38,15 +38,15 @@ dat.combine <- readRDS("../data/1582546769.28308_full_data.RData")
 #######
 rm_hub <- which(dat.combine$PROV == "Hubei")
 dat.combine <- dat.combine[-rm_hub,]
-models <- list(c("CASES_lag4"), c("CASES_lag4", "MOB"))
+models <- list(c("CASES_lag4"), c("CASES_lag4", "MOB"), c("CASES_lag4", "TEST"), c("CASES_lag4", "POPS"))
 model_names <- unlist(lapply(models, paste, collapse = "-"))
 
 results <- list()
 bic_pois <- c()
 bic_pois_mod <- c()
 bic_pois_date <- c()
-for(t in 1:length(dates)){
-  use.i <- which(dat.combine$DATE == dates[t])
+for(t in 1:(length(dates)-1)){
+  use.i <- which(dat.combine$DATE >= dates[t] & dat.combine$DATE < dates[t+1])
   dat.combine.i <- dat.combine[use.i,]
   
   BIC <- matrix(NA, ncol = 3, nrow = length(models))
@@ -103,12 +103,8 @@ for(t in 1:length(dates)){
     AIC[i,] <- c(aic.i, aic.pois.i, aic.nb.i)
     BIC[i,] <- c(bic.i, bic.pois.i, bic.nb.i)
   }
-  use.bic.i <- which.min(BIC[,2])
-  if(use.bic.i != 1 & BIC[1,2] - BIC[use.bic.i,2] < 4){
-    use.bic.i <- 1
-  }
-  
-  bic_out.i <- BIC[use.bic.i,2]
+  use.bic.i <- 1:nrow(BIC)
+  bic_out.i <-  BIC[use.bic.i[which.max(BIC[use.bic.i,2])],2] - BIC[use.bic.i,2] 
   bic_pois <- c(bic_pois, bic_out.i)
   bic_pois_mod <- c(bic_pois_mod, model_names[use.bic.i])
   bic_pois_date <- c(bic_pois_date, rep(as.character(dates[t]), length(use.bic.i)))
@@ -127,4 +123,4 @@ dat.plot <- data.frame(bic_pois, bic_pois_mod, bic_pois_date)
 dat.plot$bic_pois_date <- as.POSIXct(strptime(dat.plot$bic_pois_date, format = "%Y-%m-%d"))
 
 quartz(width = 8, height = 5)
-ggplot(dat.plot, aes(bic_pois_date, fill = bic_pois_mod)) + geom_bar() + scale_fill_manual(values = c("#0B775E", "#35274A", "#E1BD6D"), guide_legend(title = "Model"), labels = c("Cases", "Cases + Mobility")) + xlab("Date") + ylab("Relative BIC Improvement") + theme(legend.position = c(0.11,0.87), legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#EABE9495", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 14), axis.title = element_text(colour = "black", size = 15), panel.grid.minor = element_line(colour = "#00000050",linetype = 3), panel.grid.major = element_line(colour = "#00000060", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#E1BD6D",size = 1.5) + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#E1BD6D", size = 1.5) + scale_x_datetime(expand = c(0.01,0.01))
+ggplot(dat.plot, aes(as.factor(bic_pois_date), log(bic_pois), fill = bic_pois_mod)) + geom_bar(stat = "identity", position =  "dodge") + scale_fill_manual(values = c("#0B775E", "#35274A", "#E1BD6D", "#e34a33"), guide_legend(title = "Model"), labels = c("Cases", "Cases + Mobility", "Cases + Population size", "Cases + Testing")) + xlab("Date") + ylab("BIC Improvement (log scale)") + theme(legend.position = c(0.14,0.8), legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffff75", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 14), axis.title = element_text(colour = "black", size = 15), panel.grid.minor = element_line(colour = "#00000050",linetype = 3), panel.grid.major = element_line(colour = "#00000060", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) #+ geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#E1BD6D",size = 1.5) + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#E1BD6D", size = 1.5) + scale_x_datetime(expand = c(0.01,0.01))
