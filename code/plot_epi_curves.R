091e945eee95e2ca9697a6000c001d8280859a2b
scarpino <scarpino@utexas.edu>
2020-02-26 12:09:22 -0500

    adding epi curve fig and updating abstract

--- code/plot_epi_curves.R
+++ code/plot_epi_curves.R
@@ -10,7 +10,7 @@ library(ggplot2)
 ###############
 #Global Params#
 ###############
-
+date_cut <- as.POSIXct(strptime("2020-01-01", format = "%Y-%m-%d"))
 
 ######
 #Data#
@@ -20,8 +20,25 @@ dat.combine <- readRDS("../data/1582546769.28308_full_data.RData")
 ##########
 #Plotting#
 ##########
-quartz(width = 10, height = 10)
-ggplot(dat.combine, aes(DATE, log(CASES_now + 1), color = PROV)) + geom_line() + facet_wrap(~PROV) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#000000") + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")+ ylab("Daily COVID-19 cases (log scale)") + xlab("Date") + theme(legend.position = "none", legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-22", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")
+pal <-wes_palette(name = "Darjeeling1", length(unique(dat.combine$PROV)), type = "continuous")
 
 quartz(width = 10, height = 10)
-ggplot(dat.combine, aes(DATE, log(CASES_CUMULATIVE/exp(POPS)), color = PROV)) + geom_line() + facet_wrap(~PROV) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#000000") + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")+ ylab("Daily COVID-19 cases (log scale)") + xlab("Date") + theme(legend.position = "none", legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-22", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")
+ggplot(dat.combine, aes(DATE, log(CASES_now + 1), color = PROV)) + geom_line(size = 1.1) + facet_wrap(~PROV) + scale_color_manual(values = pal) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#000000") + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")+ ylab("Daily COVID-19 cases (log scale)") + xlab("2020") + theme(legend.position = "none", legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-22", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")
+
+quartz(width = 10, height = 10)
+ggplot(dat.combine, aes(DATE, log(CASES_CUMULATIVE/exp(POPS)), color = PROV)) + geom_line(size = 1.1) + facet_wrap(~PROV) + scale_color_manual(values = pal) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#000000") + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")+ ylab("Daily COVID-19 cases (log scale)") + xlab("2020") + theme(legend.position = "none", legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-22", format = "%Y-%m-%d")), linetype="dashed", color = "#000000")
+
+use_provinces <- c("Guangdong", "Fujian", "Hunan")
+dat.plot.use <- which(dat.combine$PROV %in% use_provinces & dat.combine$DATE > date_cut)
+dat.plot <- dat.combine[dat.plot.use,]
+
+pal2 <- c("#4d4d4d", wes_palette(name = "Darjeeling1", 5, type = "discrete")[c(2, 5, 1, 3, 4, 5)])
+
+quartz(width = 10, height = 6)
+ggplot(dat.plot, aes(DATE, log(CASES_now + 1), fill = PROV)) + geom_bar(stat = "identity") + scale_fill_manual(values = pal2) + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#e34a33", size = 1) + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#e34a33", size = 1)+ ylab("Daily COVID-19 cases (log scale)") + xlab("2020") + theme(legend.position = c(0.1,0.7), legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + labs(fill = "Province")
+
+rm_hub <- which(dat.combine$PROV == "Hubei" | dat.combine$DATE < date_cut)
+dat.combine.no.hub <- dat.combine[-rm_hub,]
+quartz(width = 10, height = 6)
+ggplot(dat.combine.no.hub, aes(DATE, CASES_now)) + geom_bar(stat = "identity") + geom_vline(xintercept = as.POSIXct(strptime("2020-01-23", format = "%Y-%m-%d")), linetype="dashed", color = "#e34a33", size = 1) + geom_vline(xintercept = as.POSIXct(strptime("2020-02-06", format = "%Y-%m-%d")), linetype="dashed", color = "#e34a33", size = 1)+ ylab("Daily COVID-19 cases") + xlab("2020") + theme(legend.position = c(0.1,0.7), legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000000",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) + scale_y_continuous(expand = c(0.01,0.01)) + labs(fill = "Province")
+
